48b5adb240dac6788052888930e229a039d60bf7
Legacy\Juan Pablo Lamas <lauro-jp@hotmail.com>
2019-10-02 10:04:23 -0300

    Agregado carpeta principal

---
+++ Ejercicio1/Ejercicio1/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Ejercicio nro. 1";
+            string temp;
+            int a, b, c, d, e;
+            temp = Console.ReadLine();
+            a = int.Parse(temp);
+            temp = Console.ReadLine();
+            b = int.Parse(temp);
+            temp = Console.ReadLine();
+            c = int.Parse(temp);
+            temp = Console.ReadLine();
+            d = int.Parse(temp);
+            temp = Console.ReadLine();
+            e = int.Parse(temp);
+
+            if (a> b && a > c && a > d && a > e)
+            {
+                Console.WriteLine("El primer numero es el mas grande");
+            }else if(b > c && b > d && b > e)
+            {
+                Console.WriteLine("El segundo numero es el mas grande");
+            }
+            else if (c > d && c > e)
+            {
+                Console.WriteLine("El tercer numero es el mas grande");
+            }
+            else if (d > e)
+            {
+                Console.WriteLine("El cuarto numero es el mas grande");
+            }
+            else
+            {
+                Console.WriteLine("El quinto numero es el mas grande");
+            }
+            if (a < b && a < c && a < d && a < e)
+            {
+                Console.WriteLine("El primer numero es el mas chico");
+            }
+            else if (b < c && b < d && b < e)
+            {
+                Console.WriteLine("El segundo numero es el mas chico");
+            }
+            else if (c < d && c < e)
+            {
+                Console.WriteLine("El tercer numero es el mas chico");
+            }
+            else if (d < e)
+            {
+                Console.WriteLine("El cuarto numero es el mas chico");
+            }
+            else
+            {
+                Console.WriteLine("El quinto numero es el mas chico");
+            }
+
+
+            Console.WriteLine("El promedio es " + ((a+b+c+d+e)/2));
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Ejercicio2/Ejercicio2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float num = -1;
+            while (num <= 0)
+            {
+                Console.Write("Ingrese numero:");
+                num = float.Parse(Console.ReadLine());
+                if (num <= 0)
+                {
+                    Console.WriteLine("ERROR. Reingresar numero");
+                }
+            }
+            Console.WriteLine("Potencia de 2: "+(Math.Pow(num,2))+" Potencia de 3: " + (Math.Pow(num, 3)));
+            Console.ReadKey();
+        }
+    }
+}
